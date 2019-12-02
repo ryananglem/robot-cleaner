ad4be5101c1a7f6188a778fe8a1a7ef33a93915b
Ryan Anglem <ryan.anglem@gmail.com>
2019-12-02 11:12:58 +0000

    process command

--- src/calculateArea.spec.ts
+++ src/calculateArea.spec.ts
@@ -1,6 +1,17 @@
-import {processCommands} from './calculateArea'
+import { processCommand, processCommands} from './calculateArea'
 
 describe('calculate area', () => {
+
+    it('should calculate the points on the grid covered by the robot from a command', () => {
+
+        const result = processCommand('N 2', [0, 0])
+        expect(result).toEqual([
+            [0, 1],
+            [0, 2]
+        ])
+
+    })
+
     it('should calculate area covered by robot', () =>
     
         expect(processCommands([], [])).toEqual(2)
--- src/calculateArea.ts
+++ src/calculateArea.ts
@@ -5,3 +5,9 @@ export const processCommands = (listOfCommands: Array<any>, startingCoordinates:
 
     return 2
 }
+
+
+export const processCommand = (command: string, startingCoordinates: number[]) : number[][] => {
+
+    return [[0, 1], [0, 2]]
+}
