8172cfc4db7d589c32dccacd8ada5e71ac93b8b4
Ryan Anglem <ryan.anglem@gmail.com>
2019-12-02 13:58:30 +0000

    calculate area

--- src/calculateArea.spec.ts
+++ src/calculateArea.spec.ts
@@ -1,5 +1,5 @@
 import { processCommand, processCommands} from './calculateArea'
-import { Command } from './types'
+import { Command, Direction } from './types'
 
 describe('calculate area', () => {
 
@@ -14,8 +14,28 @@ describe('calculate area', () => {
 
     })
 
-    it('should calculate area covered by robot', () =>
+    it('should calculate area covered by robot for simple journey', () => {
+        const command: Command = { direction: 'N', distance: 2} 
     
-        expect(processCommands([], [])).toEqual(2)
+        expect(processCommands([command], [0, 0])).toEqual(2)
+    }
     )
+
+    it('should calculate area covered by robot for journey revisiting places', () => {
+        const command1: Command = { direction: 'N', distance: 2} 
+        const command2: Command = { direction: 'S', distance: 1} 
+    
+        expect(processCommands([command1, command2], [0, 0])).toEqual(2)
+    }
+    )
+    it('should calculate area covered by robot for complex journey ', () => {
+        const command1: Command = { direction: 'N', distance: 2} 
+        const command2: Command = { direction: 'S', distance: 1}
+        const command3: Command = { direction: 'E', distance: 1} 
+        const command4: Command = { direction: 'W', distance: 3}
+
+        expect(processCommands([command1, command2, command3, command4], [0, 0])).toEqual(5)
+    }
+    )
+
 })
--- src/calculateArea.ts
+++ src/calculateArea.ts
@@ -2,13 +2,47 @@ import { Command } from './types'
 
 export const processCommands = (listOfCommands: Array<Command>, startingCoordinates: number[]) => {
 
-    return 2
-}
+    const placesCleaned: number[][] = []
 
+    let coordinates = startingCoordinates
+    listOfCommands.forEach(command => {
+        const placesVisited  = processCommand(command, coordinates)
+        placesVisited.forEach(place => {
+            if (!placesCleaned.find(findPlace => findPlace[0]=== place[0] && findPlace[1]==place[1])) {
+                placesCleaned.push(place)
+            }
+            coordinates = place
+        })
+        
+    })
+    return placesCleaned.length
+}
 
 export const processCommand = (command: Command, startingCoordinates: number[]) : number[][] => {
 
-
-    return [[0, 1], [0, 2]]
+    const journey: number[][] = []
+    let coordinates = startingCoordinates
+    let i=0
+    while(i < command.distance ) {
+        switch (command.direction) {
+            case 'N':
+                journey.push([ coordinates[0], coordinates[1] + 1])
+                break
+            case 'S': 
+                journey.push([ coordinates[0], coordinates[1] -1])
+                break
+            case 'E':
+                journey.push([ coordinates[0] +1, coordinates[1]])
+                break
+            case 'W':
+                journey.push([ coordinates[0] -1, coordinates[1]])
+                break
+        }
+        coordinates = journey[journey.length-1]
+        i++
+    }
+    return journey
 }
+
+
 
